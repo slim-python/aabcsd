f7353653427574d01b99eabc8df30aaedea30a1a
Abhi Raj <xl4bhi@gmail.com>
2022-11-28 15:48:04 +0530

    final commit

---
+++ project-1/src/App.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import socketIO from "socket.io-client";
+const socket = socketIO.connect("http://localhost:4000");
+function App() {
+  const [counter1, setCounter1] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+  const [counter3, setCounter3] = useState(0);
+  const counters = [
+    {
+      name: "counter1",
+      id: 111,
+      value: counter1,
+      setCounter: setCounter1,
+    },
+    {
+      name: "counter2",
+      id: 222,
+      value: counter2,
+      setCounter: setCounter2,
+    },
+    {
+      name: "counter3",
+      id: 333,
+      value: counter3,
+      setCounter: setCounter3,
+    },
+  ];
+
+  const incrementCounter = () => {
+    socket.emit("increment-counter", { counter: counters });
+  };
+
+  useEffect(() => {
+    socket.on("recieved-increment-counter", (data) => {
+      counters.forEach((counter, index) => {
+        counter.setCounter(data.counter[index].value);
+      });
+    });
+  }, [counters]);
+
+  return (
+    <div>
+      <h1>project 1</h1>
+      <div>
+        {counters.map((counter) => {
+          return (
+            <div key={counter.id}>
+              <p>{counter.name}</p>
+              <p>
+                value: <b>{counter.value}</b>
+              </p>
+              <button
+                onClick={() => {
+                  counter.setCounter(counter.value + 1);
+                  counter.value = counter.value + 1;
+                  incrementCounter();
+                }}
+              >
+                {counter.name}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+const http = require("http").Server(app);
+const cors = require("cors");
+
+app.use(cors());
+
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    methods: ["GET", "POST"],
+  },
+});
+
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+  });
+
+  socket.on("increment-counter", (data) => {
+    socket.broadcast.emit("recieved-increment-counter", data);
+  });
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Hello world",
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
